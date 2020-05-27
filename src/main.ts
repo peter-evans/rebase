cf46e175114067c4b5253fc4920f02c01990baf8
Peter Evans <peter-evans@users.noreply.github.com>
2020-05-27 18:26:19 +0900

    Execute checkout

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,29 @@
 import * as core from '@actions/core'
-import {
-  ConfigOption,
-  getRepoPath,
-  getAndUnsetConfigOption,
-  addConfigOption
-} from './git'
+import * as inputHelper from 'checkout/lib/input-helper'
+import * as gitSourceProvider from 'checkout/lib/git-source-provider'
 import {inspect} from 'util'
 
-const EXTRAHEADER_OPTION = 'http.https://github.com/.extraheader'
-const EXTRAHEADER_VALUE_REGEX = '^AUTHORIZATION:'
-
 async function run(): Promise<void> {
-  let repoPath
-  let extraHeaderOption = new ConfigOption()
   try {
     const inputs = {
       token: core.getInput('token'),
-      path: core.getInput('path'),
+      repository: core.getInput('repository'),
       base: core.getInput('base'),
       head: core.getInput('head')
     }
     core.debug(`Inputs: ${inspect(inputs)}`)
 
-    // Get the repository path
-    repoPath = getRepoPath(inputs.path)
-    // Get the extraheader config option if it exists
-    extraHeaderOption = await getAndUnsetConfigOption(
-      repoPath,
-      EXTRAHEADER_OPTION,
-      EXTRAHEADER_VALUE_REGEX
-    )
+    process.env['INPUT_PATH'] = 'somepath'
+    process.env['INPUT_REF'] = 'master'
+    process.env['INPUT_FETCH-DEPTH'] = '0'
+    process.env['INPUT_PERSIST_CREDENTIALS'] = 'true'
 
-    // Action logic
+    const sourceSettings = inputHelper.getInputs()
+    core.debug(`sourceSettings: ${inspect(sourceSettings)}`)
+
+    await gitSourceProvider.getSource(sourceSettings)
   } catch (error) {
     core.setFailed(error.message)
-  } finally {
-    // Restore the extraheader config option
-    if (extraHeaderOption.value != '') {
-      if (
-        await addConfigOption(
-          repoPath,
-          EXTRAHEADER_OPTION,
-          extraHeaderOption.value
-        )
-      )
-        core.debug(`Restored config option '${EXTRAHEADER_OPTION}'`)
-    }
   }
 }
 
