e689d72b322657d35764f8468f6d295a6e927e95
Peter Evans <peter-evans@users.noreply.github.com>
2020-06-01 18:52:33 +0900

    Remove tests

--- __test__/input-validator.unit.test.ts
+++ __test__/input-validator.unit.test.ts
@@ -1,20 +1,6 @@
 import * as inputValidator from '../lib/input-validator'
 
 describe('input validator tests', () => {
-  it('successfully parses the committer input', async () => {
-    expect(
-      inputValidator.parseCommitter('Test User <test@example.com>')
-    ).toEqual(['Test User', 'test@example.com'])
-  })
-
-  it('throws an error when unable to parse the committer input', async () => {
-    expect(() => {
-      inputValidator.parseCommitter('Test User')
-    }).toThrow(
-      `Input 'committer' does not conform to the format 'Display Name <email@address.com>'`
-    )
-  })
-
   it('successfully parses the head input', async () => {
     expect(inputValidator.parseHead('owner:ref')).toEqual(['owner', 'ref'])
   })
