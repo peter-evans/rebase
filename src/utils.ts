0c74c07f0128972917b159302c914345e694ae6a
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2022-10-17 21:03:44 +0900

    Replace use of any type

--- src/utils.ts
+++ src/utils.ts
@@ -9,13 +9,15 @@ export function fileExistsSync(path: string): boolean {
   let stats: fs.Stats
   try {
     stats = fs.statSync(path)
-  } catch (error: any) {
-    if (error.code === 'ENOENT') {
+  } catch (error) {
+    if (hasErrorCode(error) && error.code === 'ENOENT') {
       return false
     }
 
     throw new Error(
-      `Encountered an error when checking whether path '${path}' exists: ${error.message}`
+      `Encountered an error when checking whether path '${path}' exists: ${getErrorMessage(
+        error
+      )}`
     )
   }
 
@@ -38,4 +40,14 @@ function getStringAsArray(str: string): string[] {
     .split(/[\n,]+/)
     .map(s => s.trim())
     .filter(x => x !== '')
+}
+
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+function hasErrorCode(error: any): error is {code: string} {
+  return typeof (error && error.code) === 'string'
+}
+
+export function getErrorMessage(error: unknown): string {
+  if (error instanceof Error) return error.message
+  return String(error)
 }
